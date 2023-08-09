59f816cc24d64329c1c1123b6a67a40c303128ab
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-09 20:50:47 +0300

    creating authcontroller

--- instagram-clone-server/routes/api.php
+++ instagram-clone-server/routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::get('unautharized', 'unautharized')->name("unautharized");
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 });
