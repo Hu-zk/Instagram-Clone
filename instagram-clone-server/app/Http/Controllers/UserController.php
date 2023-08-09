031ecab070d2dedc42c0d7160c736fda0d7478c5
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-10 00:38:26 +0300

    creating follows api

---
+++ instagram-clone-server/app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Follower;
+
+class UserController extends Controller
+{
+    public function searchUsers($searchItem)
+    {
+
+        $auth_user = Auth::user();
+
+        $users = User::where('username', 'LIKE', "%$searchItem%")
+            ->orWhere('name', 'LIKE', "%$searchItem%")->get();
+
+        // foreach($users as $user) {
+        //     $user->is_followed_by_me = $auth_user->isFollowedBy($user);
+        // }
+
+        return response()->json(['users' => $users]);
+    }
+
+    public function follow($userId)
+    {
+        $user = Auth::user();
+
+        $isFollowed = Follower::where('follower_id', $user->id)->where('following_id', $userId)->first();
+
+        if ($isFollowed) {
+            Follower::where('follower_id', $user->id)->where('following_id', $userId)->first()->delete();
+        } else {
+            Follower::create([
+                'follower_id' => $user->id,
+                'following_id' => $userId,
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User followed/unfollowed successfully'
+        ]);
+    }
+}
