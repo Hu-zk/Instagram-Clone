a3c2f309d414112fb23f7f0fbf9f76e1d16924c7
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-10 00:44:45 +0300

    editing routes

---
+++ instagram-clone-server/app/Http/Controllers/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Like;
+
+class PostsController extends Controller
+{
+    public function createPost(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // $uploadedImage = $request->file('image');
+        // $base64Image = base64_encode(file_get_contents($uploadedImage->getRealPath()));
+
+        // $imagePath = 'post_images/' . uniqid() . '.' . $uploadedImage->getClientOriginalExtension();
+        // Storage::disk('public')->put($imagePath, base64_decode($base64Image));
+
+        $imagePath = $request->file('image')->store('post_images', 'public');
+        // echo asset('storage/post_images');
+
+
+        $post = new Post([
+            'user_id' => auth()->user()->id,
+            'image_path' => $imagePath,
+            'likes_count' => 0,
+        ]);
+        $post->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Post created successfully',
+            'post' => $post,
+        ]);
+    }
+
+    // public function like($post_id)
+    // {
+    //     $user = auth()->user();
+    //     $post = Post::find($post_id);
+
+    //     if (!$user->likes->contains($post)) {
+    //         $like = new Like([
+    //             'user_id' => $user->id,
+    //             'post_id' => $post->id,
+    //         ]);
+    //         $like->save();
+    //         $post->increment('likes_count');
+    //     }
+
+    //     return response()->json([
+    //         'status' => 'success',
+    //         'message' => 'Post liked successfully',
+    //     ]);
+    // }
+
+    // public function unlike($post_id)
+    // {
+    //     $user = auth()->user();
+    //     $post = Post::find($post_id);
+
+    //     if ($user->likes->contains($post)) {
+    //         echo $user->likes;
+    //         $user->likes->where('post_id', $post->id)->first()->delete($post);
+    //         $post->decrement('likes_count');
+    //     }
+
+    //     return response()->json([
+    //         'status' => 'success',
+    //         'message' => 'Post unliked successfully',
+    //     ]);
+    // }
+
+    public function toggleLike($postId)
+    {
+        $user = Auth::user();
+        $post = Post::find($postId);
+        $likeExists = $post->likes()->where('user_id', $user->id)->exists();
+
+        if ($likeExists) {
+            $post->likes()->where('user_id', $user->id)->delete();
+            $post->decrement('likes_count');
+        } else {
+            $like = new Like(['user_id' => $user->id]);
+            $post->likes()->save($like);
+            $post->increment('likes_count');
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'post has been liked/disliked successfully'
+        ]);
+    }
+}
--- instagram-clone-server/routes/api.php
+++ instagram-clone-server/routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -15,9 +16,8 @@ Route::middleware('auth:api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
 
-    Route::post('posts', [PostsController::class, 'createPost']);
-    Route::post('posts/{post_id}/like', [PostsController::class, 'like']);
-    Route::post('posts/{post_id}/unlike', [PostsController::class, 'unlike']);
-    Route::get('/search-users/{searchItem}', [UserController::class, 'searchUsers']);
+    Route::post('posts', [PostController::class, 'createPost']);
+    Route::get("/posts/{postId}/toggle-like", [PostController::class, 'toggleLike']);
+    Route::get('/search/{searchItem}', [UserController::class, 'searchUsers']);
     Route::get("/toggle-follow/{userId}", [UserController::class, "follow"]);
 });
